3f9dfc9ba64a879c194774dcfc0be6ab349738b2
mig-E <65431001+mig-E@users.noreply.github.com>
2020-07-16 11:06:08 -0700

    Add files via upload

---
+++ themPoker/main.py
@@ -0,0 +1,37 @@
+from poker.card import Card
+from poker.deck import Deck
+from poker.game import Game
+from poker.hand import Hand
+from poker.player import Player
+
+deck = Deck()
+cards = Card.create_standard_52_cards()
+deck.add_cards(cards)
+
+hand1 = Hand()
+hand2 = Hand()
+
+player1 = Player(name = "Miggy", hand = hand1)
+player2 = Player(name = "Hank Hill", hand = hand2)
+players = [player1, player2]
+
+game = Game(deck = deck, players = players)
+game.play()
+
+for player in players:
+    print(f"{player.name} receives a {player.hand}")
+    index, hand_name, hand_cards = player.best_hand()
+    hand_cards_strings = [str(card) for card in hand_cards]
+    hand_cards_string = " and ".join(hand_cards_strings)
+    print(f"{player.name} has a {hand_name} with a {hand_cards_string}")
+    print()
+
+winning_player = max(players)
+
+print(f"The winner is {winning_player.name}!")
+
+
+
+
+
+# from main import deck, cards, game, hand1, hand2, player1, player2
---
+++ themPoker/poker/texas_hold_em.py
@@ -0,0 +1,6 @@
+import random
+
+deck_of_cards = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace')
+
+suits = (" of Spades", " of Hearts", " of Clubs", " of Diamonds")
+
